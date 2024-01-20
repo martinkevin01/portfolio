fcc1bf5f977d32edbcc367f27585a367141bb30c
Martin Kevin Gapie <martinkevingapie@gmail.com>
2024-01-19 23:21:02 -0500

    adding about section

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,8 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def download
+    send_file "#{Rails.root}/app/assets/docs/MartinKevinGapieResumeFrench.pdf", type: "application/pdf", x_sendfile: true
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  match 'download', to: 'pages#download', as: 'download', via: :get
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
