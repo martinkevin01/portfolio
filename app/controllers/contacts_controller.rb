4920b7d4363a59d0570c003d7dc1b0cd01440c04
Martin Kevin Gapie <martinkevingapie@gmail.com>
2024-02-26 20:13:07 -0500

    creating form contact

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,17 @@
+class ContactsController < ApplicationController
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(params[:contact])
+    @contact.request = request
+    if @contact.deliver
+      # flash.now[:success] = 'Message sent!'
+      redirect_to root_path, notice: "Message sent!"
+    else
+      # flash.now[:error] = 'Could not send message'
+      render :new, alert: "Error, try again!"
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   match 'download', to: 'pages#download', as: 'download', via: :get
+  resources :contacts, only: [:new, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
