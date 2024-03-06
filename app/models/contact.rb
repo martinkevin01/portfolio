fab9db9f53065bf0a254a0794bad2075a4f1c007
Martin Kevin Gapie <martinkevingapie@gmail.com>
2024-03-05 20:39:00 -0500

    final desktop version

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,6 @@
 class Contact < MailForm::Base
-  attribute :name,      validate: true
-  attribute :email,     validate: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :name, validate: true
+  attribute :email, format: { with: /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i}
   attribute :message, validate: true
   attribute :nickname, captcha: true
 
