037f1a3302ea4ef34ed1e33a6217d533010274d7
Martin Kevin Gapie <martinkevingapie@gmail.com>
2026-02-22 20:43:59 -0500

    debug

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,7 @@
 # Configure une connexion globale à Redis.
 # En production sur Heroku, elle utilisera automatiquement la variable d'environnement REDIS_URL.
 # En développement, elle se connectera à un Redis local (si installé) ou vous devrez spécifier une URL.
-$redis = Redis.new(url: ENV["REDIS_URL"])
+$redis = Redis.new(
+  url: ENV["REDIS_URL"],
+  ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+)
